41e68e7eea4ab008d95d1fb6ed638689d6e0499d
freezing1116 <freezing16@naver.com>
2025-04-12 14:04:54 +0900

    shell code for testing

--- test_shell/test_bubble.sh
+++ test_shell/test_bubble.sh
@@ -28,28 +28,28 @@ mkdir -p results
 
 for algo in "${algorithms[@]}"; do
     if [[ ! -x ./$algo ]]; then
-        echo "❌ impossible to run './$algo'"
+        echo "❌ cannot run './$algo'"
         continue
     fi
 
+    csv="results/${algo}.csv"
+    echo "testcase,time(ms)" > "$csv"
+
     for dir in "${testcase_dirs[@]}"; do
         if [[ ! -d $dir ]]; then
-            echo "❌ no directory '$dir' existed"
+            echo "❌ directory '$dir' does not exist"
             continue
         fi
 
         for input in "$dir"/*.txt; do
-            base_name=$(basename "$input" .txt)               # test_asce_1K
-            csv="results/${algo}_${base_name}.csv"            # results/library_sort_test_asce_1K.csv
-
-            echo "testcase,time(ms)" > "$csv"
-            echo "▶️  $algo on $input"
+            base_name=$(basename "$input" .txt)
+            echo "▶️  Running $algo on $base_name"
 
             TIMEFORMAT="%R"
             time_in_seconds=$( { time ./$algo "$input" > /dev/null; } 2>&1 )
             time_in_ms=$(echo "$time_in_seconds * 1000" | bc)
 
-            echo "$base_name,$time_in_ms" >> "$csv"
-        done
+        echo "$base_name,$time_in_ms" >> "$csv"
     done
+done
 done
